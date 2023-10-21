01ed641832d49f754aad4b4836c3d6477a3576d8
Mark Delany <puz@bravo.emu.st>
2023-10-21 15:28:56 +0200

    Benchmark the Marshal and Unmarshal convenience functions. (3-4 times slower, as expected).

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -3,6 +3,7 @@ package netstring_test
 import (
 	"bytes"
 	"io"
+	"strconv"
 	"testing"
 
 	"github.com/markdingo/netstring"
@@ -116,6 +117,131 @@ func BenchmarkCompareDecode(b *testing.B) {
 			}
 			if len(buf) != 10 {
 				b.Fatal("Wrong length. Expected 10, got", len(buf))
+			}
+		}
+	}
+}
+
+type bmStruct struct {
+	Age         int    `netstring:"a"`
+	Country     string `netstring:"c"`
+	TLD         []byte `netstring:"t"`
+	CountryCode []byte `netstring:"C"`
+	Name        string `netstring:"n"`
+	Height      uint16 `netstring:"H"`
+	Key         int64  `netstring:"K"`
+}
+
+// The higher-level Marshal function turns out to be about 4 times slower than manually encoding.
+func BenchmarkMarshalAuto(b *testing.B) {
+	s := bmStruct{21, "Iceland", []byte{'i', 'c'}, []byte("354"), "Bjorn", 183, 123456}
+	enc := netstring.NewEncoder(&benchNullWriter{})
+	for i := 0; i < b.N; i++ {
+		err := enc.Marshal('Z', s)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// Do what MarshalAuto does, but using the simpler and lower-level Encoders directly. The
+// extra cost of Marshal is largely due to the tag fetching, deduping and reflection
+// aspects.
+func BenchmarkMarshalManual(b *testing.B) {
+	s := bmStruct{21, "Australia", []byte{'a', 'u'}, []byte("354"), "Bruce", 200, 987654}
+	enc := netstring.NewEncoder(&benchNullWriter{})
+	for i := 0; i < b.N; i++ {
+		err := enc.EncodeInt('a', s.Age)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeString('c', s.Country)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeBytes('t', s.TLD)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeBytes('C', s.CountryCode)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeString('n', s.Name)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeUint('H', uint(s.Height))
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeInt64('K', s.Key)
+		if err != nil {
+			b.Fatal(err)
+		}
+		err = enc.EncodeBytes('Z')
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// As with marshal, the higher levvel Unmarshal turns out to be about 3-4 times slower
+// than manually decoding.
+func BenchmarkUnmarshalAuto(b *testing.B) {
+	in := "3:a99,10:cAustralia,3:tau,4:C354,6:nBruce,4:H200,7:K987654,1:Z,"
+	rBuf := bytes.NewReader([]byte(in))
+	for i := 0; i < b.N; i++ {
+		rBuf.Seek(0, io.SeekStart)
+		dec := netstring.NewDecoder(rBuf)
+		var s bmStruct
+		unk, err := dec.Unmarshal('Z', &s)
+		if err != nil {
+			b.Fatal("iter", i, "unmarshal returned", err)
+		}
+		if unk != 0 {
+			b.Fatal("Unknown key returned", unk)
+		}
+	}
+}
+
+// Do what UnmarshalAuto does, but using the simpler and lower-level Decoders directly.
+func BenchmarkUnmarshalManual(b *testing.B) {
+	in := "3:a99,10:cAustralia,3:tau,4:C354,6:nBruce,4:H200,7:K987654,1:Z,"
+	rBuf := bytes.NewReader([]byte(in))
+	for i := 0; i < b.N; i++ {
+		rBuf.Seek(0, io.SeekStart)
+		dec := netstring.NewDecoder(rBuf)
+		var s bmStruct
+		for {
+			k, v, e := dec.DecodeKeyed()
+			if e != nil {
+				b.Fatal(e)
+			}
+
+			if k == 'Z' {
+				break
+			}
+
+			switch k {
+			case 'a':
+				s.Age, _ = strconv.Atoi(string(v))
+			case 'c':
+				s.Country = string(v)
+			case 't':
+				s.TLD = v
+			case 'C':
+				s.CountryCode = v
+			case 'n':
+				s.Name = string(v)
+			case 'H':
+				u16, _ := strconv.Atoi(string(v))
+				s.Height = uint16(u16)
+			case 'K':
+				i64, _ := strconv.Atoi(string(v))
+				s.Key = int64(i64)
+			default:
+				b.Fatal("Unexpected Key type", k)
 			}
 		}
 	}
